8610fc44521f06ecfcf2303910d56a004de6a569
tejapathuri <112194025+tejapathuri@users.noreply.github.com>
2023-04-30 20:53:06 -0500

    delete

--- DogsManagementSystem/src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bandiClasses.DMS.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bandiClasses.DMS.Models.Trainer;
-
-public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
-
-}
