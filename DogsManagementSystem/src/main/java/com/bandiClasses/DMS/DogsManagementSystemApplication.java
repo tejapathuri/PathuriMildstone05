453b1c5fa4c1ce8cd6d3af0170c6b590caec8b2b
tejapathuri <tejatheyellowarmy@gmail.com>
2023-04-30 21:27:52 -0500

    added dockerfile

---
+++ DogsManagementSystem/src/main/java/com/bandiClasses/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,17 @@
+package com.bandiClasses.DMS;
+
+/**
+ * @author Tejeswara Rao Pathuri
+ *
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
